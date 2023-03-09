a13c476113d83b4fa7ef662f6b63dbb556bfcaa4
Łukasz Wala <lukasz.wala@swmansion.com>
2023-03-09 16:29:30 +0100

    Refactor structure accordingly to @mickel8 suggestions

--- lib/jellyfish/client.ex
+++ lib/jellyfish/client.ex
@@ -1,7 +1,29 @@
 defmodule Jellyfish.Client do
   @moduledoc """
-  Defines a `t:Jellyfish.SDK.Client.t/0`.
+  Allows to create client, then use it to create, delete or list rooms.
+  One client represents one instance of Jellyfish.
+
+  ## Examples
+  ```
+  iex> client = Jellyfish.Client.new("http://addresstojellyfish.com")  => %Jellyfish.Client{...}
+
+  iex> {:ok, room} = Jellyfish.Client.create_room(client, max_peers: 10)
+  {:ok,
+    %Jellyfish.Room{
+      components: [],
+      config: %{max_peers: 10},
+      id: "d3af274a-c975-4876-9e1c-4714da0249b8",
+      peers: []
+  }}
+
+  iex> :ok = Jellyfish.Client.delete_room(client, room.id)
+  :ok
+  ```
   """
+
+  alias Jellyfish.Exception.ResponseStructureError
+  alias Jellyfish.{Room, Utils}
+  alias Tesla.Env
 
   @enforce_keys [
     :http_client
@@ -13,7 +35,7 @@ defmodule Jellyfish.Client do
         }
 
   @doc """
-  Creates new instance of `t:Jellyfish.SDK.Client.t/0`.
+  Creates new instance of `t:Jellyfish.Client.t/0`.
 
   ## Parameters
 
@@ -30,5 +52,68 @@ defmodule Jellyfish.Client do
     http_client = Tesla.client(middleware, adapter)
 
     %__MODULE__{http_client: http_client}
+  end
+
+  @doc """
+  List metadata of all of the rooms.
+  """
+  @spec list_rooms(t()) :: {:ok, [Room.t()]} | {:error, atom() | String.t()}
+  def list_rooms(client) do
+    with {:ok, %Env{status: 200, body: body}} <- Tesla.get(client.http_client, "/room"),
+         {:ok, data} <- Map.fetch(body, "data"),
+         result <- Enum.map(data, &Utils.room_from_json/1) do
+      {:ok, result}
+    else
+      :error -> raise ResponseStructureError
+      error -> Utils.handle_response_error(error)
+    end
+  end
+
+  @doc """
+  Get metadata of the room with `room_id`.
+  """
+  @spec get_room_by_id(t(), Room.id()) :: {:ok, Room.t()} | {:error, atom() | String.t()}
+  def get_room_by_id(client, room_id) do
+    with {:ok, %Env{status: 200, body: body}} <-
+           Tesla.get(client.http_client, "/room/#{room_id}"),
+         {:ok, data} <- Map.fetch(body, "data"),
+         result <- Utils.room_from_json(data) do
+      {:ok, result}
+    else
+      :error -> raise ResponseStructureError
+      error -> Utils.handle_response_error(error)
+    end
+  end
+
+  @doc """
+  Create a room.
+  """
+  @spec create_room(t(), Room.options()) :: {:ok, Room.t()} | {:error, atom() | String.t()}
+  def create_room(client, opts \\ []) do
+    with {:ok, %Env{status: 201, body: body}} <-
+           Tesla.post(
+             client.http_client,
+             "/room",
+             %{"maxPeers" => Keyword.get(opts, :max_peers)},
+             headers: [{"content-type", "application/json"}]
+           ),
+         {:ok, data} <- Map.fetch(body, "data"),
+         result <- Utils.room_from_json(data) do
+      {:ok, result}
+    else
+      :error -> raise ResponseStructureError
+      error -> Utils.handle_response_error(error)
+    end
+  end
+
+  @doc """
+  Delete the room with `room_id`.
+  """
+  @spec delete_room(t(), Room.id()) :: :ok | {:error, atom() | String.t()}
+  def delete_room(client, room_id) do
+    case Tesla.delete(client.http_client, "/room/#{room_id}") do
+      {:ok, %Env{status: 204}} -> :ok
+      error -> Utils.handle_response_error(error)
+    end
   end
 end
--- test/jellyfish/room_test.exs
+++ test/jellyfish/room_test.exs
@@ -6,7 +6,6 @@ defmodule Jellyfish.RoomTest do
   alias Jellyfish.{Client, Component, Peer, Room}
 
   @url "http://mockurl.com"
-  @invalid_url "http://invalid-url.com"
 
   @component_id "mock_component_id"
   @component_type "hls"
@@ -14,11 +13,6 @@ defmodule Jellyfish.RoomTest do
   @peer_type "webrtc"
 
   @room_id "mock_room_id"
-
-  @max_peers 10
-
-  @invalid_room_id "invalid_mock_room_id"
-  @invalid_max_peers "abc"
 
   @invalid_peer_id "invalid_peer_id"
   @invalid_peer_type "abc"
@@ -38,129 +32,6 @@ defmodule Jellyfish.RoomTest do
     http_client = Tesla.client(middleware, adapter)
 
     %{client: %Client{http_client: http_client}}
-  end
-
-  describe "Room.create/2" do
-    setup do
-      valid_body = Jason.encode!(%{"maxPeers" => @max_peers})
-      invalid_body = Jason.encode!(%{"maxPeers" => @invalid_max_peers})
-
-      mock(fn
-        %{
-          method: :post,
-          url: "#{@url}/room",
-          body: ^valid_body
-        } ->
-          json(%{"data" => build_room_json(true)}, status: 201)
-
-        %{
-          method: :post,
-          url: "#{@url}/room",
-          body: ^invalid_body
-        } ->
-          json(%{"errors" => @error_message}, status: 400)
-      end)
-    end
-
-    test "when request is valid", %{client: client} do
-      assert {:ok, room} = Room.create(client, max_peers: @max_peers)
-      assert room == build_room(true)
-    end
-
-    test "when request is invalid", %{client: client} do
-      assert {:error, "Request failed: #{@error_message}"} =
-               Room.create(client, max_peers: @invalid_max_peers)
-    end
-  end
-
-  describe "Room.delete/2" do
-    setup do
-      mock(fn
-        %{
-          method: :delete,
-          url: "#{@url}/room/#{@room_id}"
-        } ->
-          text("", status: 204)
-
-        %{
-          method: :delete,
-          url: "#{@url}/room/#{@invalid_room_id}"
-        } ->
-          json(%{"errors" => @error_message}, status: 404)
-      end)
-    end
-
-    test "when request is valid", %{client: client} do
-      assert :ok = Room.delete(client, @room_id)
-    end
-
-    test "when request is invalid", %{client: client} do
-      assert {:error, "Request failed: #{@error_message}"} = Room.delete(client, @invalid_room_id)
-    end
-  end
-
-  describe "Room.list/1" do
-    setup do
-      mock(fn
-        %{
-          method: :get,
-          url: "#{@url}/room"
-        } ->
-          json(%{"data" => [build_room_json(false)]}, status: 200)
-
-        %{
-          method: :get,
-          url: "#{@invalid_url}/room"
-        } ->
-          %Tesla.Env{status: 404, body: nil}
-      end)
-    end
-
-    test "when request is valid", %{client: client} do
-      assert {:ok, rooms} = Room.list(client)
-      assert rooms == [build_room(false)]
-    end
-
-    test "when request is invalid" do
-      middleware = [
-        {Tesla.Middleware.BaseUrl, @invalid_url},
-        Tesla.Middleware.JSON
-      ]
-
-      adapter = Tesla.Mock
-      http_client = Tesla.client(middleware, adapter)
-      invalid_client = %Client{http_client: http_client}
-
-      assert_raise Jellyfish.Exception.ResponseStructureError, fn -> Room.list(invalid_client) end
-    end
-  end
-
-  describe "Room.get_by_id/2" do
-    setup do
-      mock(fn
-        %{
-          method: :get,
-          url: "#{@url}/room/#{@room_id}"
-        } ->
-          json(%{"data" => build_room_json(false)}, status: 200)
-
-        %{
-          method: :get,
-          url: "#{@url}/room/#{@invalid_room_id}"
-        } ->
-          json(%{"errors" => @error_message}, status: 404)
-      end)
-    end
-
-    test "when request is valid", %{client: client} do
-      assert {:ok, room} = Room.get_by_id(client, @room_id)
-      assert room == build_room(false)
-    end
-
-    test "when request is invalid", %{client: client} do
-      assert {:error, "Request failed: #{@error_message}"} =
-               Room.get_by_id(client, @invalid_room_id)
-    end
   end
 
   describe "Room.add_component/4" do
@@ -281,26 +152,6 @@ defmodule Jellyfish.RoomTest do
       assert {:error, "Request failed: #{@error_message}"} =
                Room.delete_peer(client, @room_id, @invalid_peer_id)
     end
-  end
-
-  defp build_room(empty?) do
-    %Room{
-      id: @room_id,
-      config: %{max_peers: @max_peers},
-      components:
-        if(empty?, do: [], else: [%Component{id: @component_id, type: @component_type}]),
-      peers: if(empty?, do: [], else: [%Peer{id: @peer_id, type: @peer_type}])
-    }
-  end
-
-  defp build_room_json(empty?) do
-    %{
-      "id" => @room_id,
-      "config" => %{"maxPeers" => @max_peers},
-      "components" =>
-        if(empty?, do: [], else: [%{"id" => @component_id, "type" => @component_type}]),
-      "peers" => if(empty?, do: [], else: [%{"id" => @peer_id, "type" => @peer_type}])
-    }
   end
 
   defp build_component() do
