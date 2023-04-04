2cb4fee24da85aac411aca9668e08a08554cb706
Przemysław Rożnawski <48837433+shuntrho@users.noreply.github.com>
2023-04-04 10:27:58 +0200

    Delete test_with_jelly.exs

--- test_with_jelly.exs
+++
@@ -1,9 +0,0 @@
-alias Jellyfish.{Client, Peer, Room}
-
-client = Client.new("http://localhost:4000")
-
-{:ok, %Room{id: room_id}} = Room.create(client, max_peers: 10)
-
-{:ok, %Peer{id: peer_id}, auth_token} = Room.add_peer(client, room_id, "webrtc") |> IO.inspect()
-
-IO.inspect(auth_token, label: :token)
