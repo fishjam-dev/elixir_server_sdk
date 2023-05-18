29e47647301808942e2db297a888eb837c869093
Radosław Szuma <radoslawszuma@gmail.com>
2023-05-18 07:02:34 +0200

    Fixes after review

--- compile_proto.sh
+++ compile_proto.sh
@@ -9,7 +9,9 @@ git submodule sync --recursive >> /dev/null
 git submodule update --recursive --remote --init >> /dev/null
 printf "DONE\n\n"
 
-file=$(find protos/jellyfish -name "server_notifications.proto")
+file="./protos/jellyfish/server_notifications.proto"
+printf "Compiling: file $file\n"
 protoc --elixir_out=./lib/ $file
+printf "DONE\n"
 
 mix format "lib/protos/**/*.ex"
