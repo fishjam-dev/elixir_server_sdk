7e8498503207fc32391c192f03c379492160b313
Radosław Szuma <56085570+Rados13@users.noreply.github.com>
2023-05-18 06:07:27 +0100

    Update protos (#14)
    * Update protos
    
    * Include only server notifications
    
    * Fixes after review

--- compile_proto.sh
+++ compile_proto.sh
@@ -9,16 +9,9 @@ git submodule sync --recursive >> /dev/null
 git submodule update --recursive --remote --init >> /dev/null
 printf "DONE\n\n"
 
-files=$(find protos/jellyfish -name "*.proto")
-
-printf "Compiling:\n"
-count=1
-total=${#files[@]}
-for file in $files; do
-  printf "Compile file %s %s ... " $count $file
-  protoc --elixir_out=./lib/ $file
-  printf "DONE\n"
-  count=$(($count + 1))
-done
+file="./protos/jellyfish/server_notifications.proto"
+printf "Compiling: file $file\n"
+protoc --elixir_out=./lib/ $file
+printf "DONE\n"
 
 mix format "lib/protos/**/*.ex"
