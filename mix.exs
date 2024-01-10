4b51aa9b9084b21414e4ce062f16065f8f13a005
Przemysław Rożnawski <roznawski.przemyslaw@gmail.com>
2024-01-10 12:11:31 +0100

    Split local and docker

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule Membrane.Template.Mixfile do
         "coveralls.post": :test,
         "coveralls.html": :test,
         "coveralls.json": :test,
-        "test.local": :test
+        "test.docker": :test
       ]
     ]
   end
@@ -115,62 +115,8 @@ defmodule Membrane.Template.Mixfile do
 
   def aliases do
     [
-      test: &test_in_docker/1,
-      "test.local": ["cmd MIX_ENV=test_local mix test --without_docker"]
+      "test.local": ["cmd MIX_ENV=test_local mix test"],
+      "test.docker": "test_with_docker"
     ]
-  end
-
-  defp test_in_docker(opts) do
-    cond do
-      List.last(opts) == "--without_docker" ->
-        IO.puts("Running tests locally (it requires setting up jellyfish without docker) ...")
-
-        Mix.Task.run("test", ["--exclude", "file_component_sources"])
-
-      System.find_executable("docker") ->
-        IO.puts("Running tests using Docker. To run tests without Docker call \"mix test.local\"")
-
-        docker_compose_prefix = ["docker", "compose", "-f", "docker-compose-test.yaml"]
-
-        stream_command(docker_compose_prefix ++ ["pull"])
-
-        stream_command(
-          docker_compose_prefix ++ ["up", "--remove-orphans", "test", "--exit-code-from", "test"]
-        )
-
-        stream_command(docker_compose_prefix ++ ["down"])
-
-      true ->
-        IO.puts("Running tests inside docker container ...")
-
-        Mix.Task.run("test")
-    end
-  end
-
-  defp stream_command(cmd) do
-    port =
-      Port.open({:spawn, Enum.join(cmd, " ")}, [
-        {:line, 1024},
-        :use_stdio,
-        :stderr_to_stdout,
-        :exit_status
-      ])
-
-    receive_and_print(port)
-  end
-
-  defp receive_and_print(port) do
-    receive do
-      {^port, {:data, {:eol, line}}} ->
-        IO.puts(line)
-        receive_and_print(port)
-
-      {^port, {:data, data}} ->
-        IO.puts(data)
-        receive_and_print(port)
-
-      {^port, {:exit_status, exit_status}} ->
-        IO.puts("Docker command exited with status code: #{exit_status}")
-    end
   end
 end
