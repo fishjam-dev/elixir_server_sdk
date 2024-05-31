ee8a2fbf6668570aa754d95a657aa876ce9ba1ef
Przemysław Rożnawski <48837433+roznawsk@users.noreply.github.com>
2024-05-31 15:28:35 +0200

    Update example (#69)

--- examples/server_socket.exs
+++ examples/server_socket.exs
@@ -11,19 +11,21 @@ server_api_token = "development"
 {:ok, notifier} =
   Fishjam.WSNotifier.start(server_address: server_address, server_api_token: server_api_token)
 
-{:ok, _rooms} = Fishjam.WSNotifier.subscribe_server_notifications(notifier, :all)
+:ok = Fishjam.WSNotifier.subscribe_server_notifications(notifier)
 :ok = Fishjam.WSNotifier.subscribe_metrics(notifier)
 
-receive_notification = fn receive_notification ->
-  receive do
-    {:fishjam, event} ->
-      IO.inspect(event, label: :event)
-  after
-    150_000 ->
-      IO.inspect(:timeout)
-  end
+defmodule Notification do
+  def receive() do
+    receive do
+      {:fishjam, event} ->
+        IO.inspect(event, label: :event)
+    after
+      15_000 ->
+        IO.inspect(:timeout)
+    end
 
-  receive_notification.(receive_notification)
+    receive()
+  end
 end
 
-receive_notification.(receive_notification)
+Notification.receive()
